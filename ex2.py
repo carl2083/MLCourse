374eeb7d3a5224423f8ffea46e8230454d479c7e
carl <carlchanccx@gmail.com>
2019-11-21 13:28:59 +1100

    working script, only thing not working is the plot , could not get plot decision boundary to work

--- ex2.py
+++ ex2.py
@@ -10,12 +10,11 @@ def sigmoid(a):
 
 
 def computeCost (theta,X,y,lbda):
-    print(theta.shape)
     m = X.shape[0]
     hx = sigmoid(X @ theta)
-    J = (1/m) * np.sum(np.multiply(-y, np.log(hx)) - np.multiply ((1-y),np.log(1-hx)))
+    temp = (1/m) * (-y.T @ np.log(hx) - (1-y).T @ np.log(1-hx))
     reg = (lbda/(2*m)) * theta[1:].T @ theta[1:]
-    J = J + reg
+    J = temp + reg
     return J
 def gradient(theta,X,y, lbda):
     m = X.shape[0]
@@ -23,15 +22,41 @@ def gradient(theta,X,y, lbda):
     theta[0] = theta[0] - lbda/m @ theta[0]
     return grad
 
+
+def plotData():
+    plt.figure(figsize=(10, 6))
+    plt.plot(pos[:, 1], pos[:, 2], 'k+', label='Admitted')
+    plt.plot(neg[:, 1], neg[:, 2], 'yo', label='Not admitted')
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+
+    boundary_xs = np.array([np.min(X[:, 1])], np.max(X[:, 1]))
+    boundary_ys = (-1. / newTheta[2]) * (newTheta[0] + newTheta[1] * boundary_xs)
+    plt.plot(boundary_xs, boundary_ys, 'b-', label="Decision Boundary")
+    plt.legend()
+    plt.grid(True)
+
 X = data[:,0:2] # 100,2
 y = data[:,2] # 100,
 ones = np.ones((X.shape[0],1))
 X = np.hstack((ones,X)) # 100,3
 y = y[:,np.newaxis] # 100,1
 theta = np.zeros((3,1)) # 3,1
-
 lbda = 0
+print("Before optimise", computeCost(theta,X,y,lbda))
+pos = np.array([X[i] for i in range(X.shape[0]) if y[i] == 1])
+neg = np.array([X[i] for i in range(X.shape[0]) if y[i] == 0])
 result = opt.fmin(computeCost,x0=theta,args=(X,y,lbda),maxiter=400, full_output=True)
+newTheta = result[0]
+print(newTheta)
+print("After optimise", computeCost(newTheta,X,y,lbda))
+
+
+plotData()
+
+
+plt.show()
+
 
 # J = computeCost(X,y,theta)
 # print(J)
